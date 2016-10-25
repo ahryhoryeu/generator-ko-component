c7f5128be8dca9db2ec8b24d93434261fab965ef
Andrei Hryhoryeu <andrei.hryhoryeu@itechart-group.com>
2016-10-25 12:23:05 +0300

    Updated index.js to ES6 and added separate view model file generation.

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,68 +1,112 @@
 'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+let yeoman = require('yeoman-generator');
+let chalk = require('chalk');
+let yosay = require('yosay');
 
 module.exports = yeoman.Base.extend({
 
-  prompting: function () {
-    var done = this.async();
+    prompting: function () {
+        let done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the ' + chalk.red('generator-ko-component') + ' generator!'
-    ));
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            `Welcome to the ${chalk.red('knockout.js component')} generator!`
+        ));
 
-    var prompts = [
-        {
-            type: 'input',
-            name: 'componentName',
-            message: 'Component name?',
-            default: 'test'
-        },
-        {
-            type: 'confirm',
-            name: 'generateLess',
-            message: 'Do you want to include less stylesheet?',
-            default: true
+        let prompts = [
+            {
+                type: 'input',
+                name: 'componentName',
+                message: 'Component name?',
+                default: 'test'
+            },
+            {
+                type: 'confirm',
+                name: 'generateLess',
+                message: 'Do you want to include less stylesheet?',
+                default: true
+            },
+            {
+                type: 'confirm',
+                name: 'addViewModel',
+                message: 'Do you want to add View Model class?',
+                default: true
+            },
+            {
+                type: 'confirm',
+                name: 'hasSeparateViewModel',
+                message: 'Do you want Component View Model to be in a separate file?',
+                default: false
+            },
+            {
+                type: 'input',
+                name: 'basePath',
+                message: 'Provide base path (optional)'
+            }
+        ];
+
+        this.prompt(prompts, props => {
+            this.props = props;
+            // To access props later use this.props.someAnswer;
+            done();
+        });
+    },
+
+    writing: function () {
+
+        let filePath = `${this.props.componentName}/${this.props.componentName}`;
+        let userPath = this.props.basePath || this.destinationRoot();
+        let basePath = `${userPath}/${filePath}`;
+
+        let toTitleCase = function (str) {
+            return str.split('')
+                .map((char, i) => i === 0 ? char.toUpperCase() : char)
+                .join('');
+        };
+
+        let viewModelName = this.props.componentName.split('-')
+            .map(x => toTitleCase(x))
+            .join('');
+
+        viewModelName = `${viewModelName}ViewModel`;
+
+        this.fs.copyTpl(
+            this.templatePath('component.html'),
+            this.destinationPath(filePath + '.html'),
+            {
+                componentName: this.props.componentName
+            }
+        );
+
+        this.fs.copyTpl(
+            this.templatePath('component.js'),
+            this.destinationPath(filePath + '.js'),
+            {
+                addViewModel: this.props.addViewModel,
+                separateViewModel: this.props.hasSeparateViewModel,
+                viewModelName: viewModelName,
+                basePath: basePath
+            }
+        );
+
+        if (this.props.hasSeparateViewModel) {
+            this.fs.copyTpl(
+                this.templatePath('component-view-model.js'),
+                this.destinationPath(filePath + '-view-model.js'),
+                {
+                    viewModelName: viewModelName
+                }
+            )
         }
-    ];
 
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someAnswer;
-
-      this.mkdir(this.props.componentName);
-      done();
-    }.bind(this));
-  },
-
-  writing: function () {
-
-    var filePath = this.props.componentName + '/' + this.props.componentName;
-
-    this.fs.copy(
-      this.templatePath('component.html'),
-      this.destinationPath(filePath + '.html')
-    );
-
-    this.fs.copy(
-      this.templatePath('component.js'),
-      this.destinationPath(filePath + '.js')
-    );
-
-    if (this.props.generateLess) {
-      this.fs.copyTpl(
-        this.templatePath('component.less'),
-        this.destinationPath(filePath + '.less'),
-        {
-          componentName: this.props.componentName
+        if (this.props.generateLess) {
+            this.fs.copyTpl(
+                this.templatePath('component.less'),
+                this.destinationPath(filePath + '.less'),
+                {
+                    componentName: this.props.componentName
+                }
+            );
         }
-      );
     }
-  },
-
-  install: function () {
-    //this.installDependencies();
-  }
 });
--- generators/app/templates/component.js
+++ generators/app/templates/component.js
@@ -1,9 +1,15 @@
 define(function (require) {
     'use strict';
 
-    var template = require('text!');
+    var template = require('text!<%= basePath %>.html');
+    <% if (addViewModel && separateViewModel) {%>var <%= viewModelName %> = require('<%= basePath %>-view-model');<%}%>
+    <% if(addViewModel && !separateViewModel) {%>
+    function <%= viewModelName %>(params) {
 
+    }
+    <%} %>
     return {
-        template: template
+        template: template<% if(addViewModel) {%>,
+        viewModel: <%= viewModelName %><%} %>
     };
 });
